0a60f88205776403b0574244bd460b09dc70ce68
abdul kabugu <kabuguabdul2@gmail.com>
2023-01-07 16:23:18 +0300

    Fix : cleared  errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import {Provider, useDispatch, useSelector} from 'react-redux'
 import { store } from '../redux/store'
 import OrbisProvider from '../context/orbisProvider'
 import {PINATA_GATEWAY,PINATA_KEY,PINATA_SECRET} from '../assets/constants'
-import {Orbis} from '@orbisclub/orbis-sdk'
 import {configureChains, createClient, WagmiConfig} from 'wagmi'
 import {mainnet, polygonMumbai, polygon} from 'wagmi/chains'
 import {publicProvider} from 'wagmi/providers/public'
@@ -24,12 +23,6 @@ const client = createClient({
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  const orbis = new Orbis({
-    PINATA_GATEWAY: PINATA_GATEWAY,
-    PINATA_API_KEY: PINATA_KEY,
-    PINATA_SECRET_API_KEY: PINATA_SECRET
-  }
-  )
    
   return (
     <Provider store={store}>
